4ee27d3ef0be559755be20b52e8207cc8de0b951
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-07-27 01:18:16 +0300

    Enter

--- Models/Product.cs
+++ Models/Product.cs
@@ -9,7 +9,7 @@
 
         public decimal Price { get; set; }
 
-        public string ImageUrl { get; set; } 
+        public string ImageUrl { get; set; }
         
         public string Description { get; set; }
     }
