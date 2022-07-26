faf29754011a8bbedb2a8fcda73bb9d6f3a1b237
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-07-27 00:26:32 +0300

    Migrations

--- Models/Product.cs
+++ Models/Product.cs
@@ -9,6 +9,8 @@
 
         public decimal Price { get; set; }
 
-        public string ImageUrl { get; set; }    
+        public string ImageUrl { get; set; } 
+        
+        public string Description { get; set; }
     }
 }
